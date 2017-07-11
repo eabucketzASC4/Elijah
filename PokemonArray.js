8afcc050276bfaff485e9885cd513b9da0f6a456
eabucketzASC4 <elijah_atwell@woodberry.org>
2017-07-11 12:08:05 -0400

    first commit

---
+++ PokemonArray.js
@@ -0,0 +1,9 @@
+var myPokemonRoster = ["Scizor","Charizard","Turtwig","Blastoise","Ninetales"];
+var firsttofight = myPokemonRoster[0]
+var secondtofight = myPokemonRoster[1]
+myPokemonRoster.pop()
+
+var myPokemonRoster = ["Scizor","Charizard","Turtwig","Blastoise","Ninetales"];
+for (var i = 0; i < 5 ; i++){
+    console.log(myPokemonRoster[i]);
+}
