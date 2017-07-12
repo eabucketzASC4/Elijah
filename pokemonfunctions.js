0a68449a2cb5fe495dac3ed8525541c0fd6b58f5
eabucketzASC4 <elijah_atwell@woodberry.org>
2017-07-12 13:23:05 -0400

    Elijah

---
+++ pokemonfunctions.js
@@ -0,0 +1,12 @@
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+function randLetter(){
+    return alphabet[Math.floor(Math.random()*alphabet.length)]
+}
+console.log(randLetter());
+
+function randWord(){
+   //Write a function called randWord() that returns a string of random length of random letters
+
+   var wordLength = Math.floor(Math.random()*10)
+   return wordLength = 
+}
