ff5b9c02dc7f6b02744d076db749bcdab1c146f7
Elijah Atwell <elijah_atwell@woodberry.org>
2017-08-07 08:50:53 -0700

    Goat

---
+++ BrainTeaser.js
@@ -0,0 +1,9 @@
+var dictionary = ["apple","banana","carrot"]
+
+function find (word) {
+    for(i=0;i<dictionary.length;i++)
+    if(word === dictionary[i]){
+        return(true);
+    }
+}
+retrun false;
