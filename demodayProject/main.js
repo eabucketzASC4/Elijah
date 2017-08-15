4095a910ef05c9bc8636addb21f894741b96f7c1
Elijah Atwell <elijah_atwell@woodberry.org>
2017-08-15 10:39:01 -0700

    searchbar

--- demodayProject/main.js
+++ demodayProject/main.js
@@ -1,33 +1,10 @@
-$('.like-btn').on('click', function() {
-   $(this).toggleClass('is-active');
-});
-$('.minus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value &gt; 1) {
-        value = value - 1;
-    } else {
-        value = 0;
+function search() {
+    var database = firebase.database().ref();
+    var search = $("#search").val();
+   console.log(search);
+    var data = {
+        "search": search,
     }
- 
-  $input.val(value);
- 
-});
- 
-$('.plus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value &lt; 100) {
-        value = value + 1;
-    } else {
-        value =100;
+    database.push(data);
     }
- 
-    $input.val(value);
-});
+
